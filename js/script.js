a3a63123b4f05ae065a92328531e9fd9c483b894
Alex Albaran <albaranlex@icloud.com>
2021-03-25 15:00:15 +0100

    added contact section

--- js/script.js
+++ js/script.js
@@ -11,3 +11,13 @@ function changeImg(image){
     var imghol = document.getElementById("imageHolder");
     imghol.src = image;
   }
+
+/* SUBMIT FORM */
+  function openForm() {
+    document.getElementById("my-form").style.width = "500px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeForm() {
+    document.getElementById("my-form").style.width = "0";
+  }
